08d021912048e9367abef49c6b342148c140bb51
Vivek Mishra <vivekunofficialmail@gmail.com>
2025-03-26 21:12:56 +0530

    update code

--- Backend/Admin/admin_Route.js
+++ Backend/Admin/admin_Route.js
@@ -13,13 +13,13 @@ const auth = require("../Middleware/Authentication");
 
 const router = express.Router();
 router.post("/register",  registerAdmin);
-router.post("/login",loginuser);
-// router.post("/add", auth, regi_user); // Only admin can add users
+router.post("/adminLogin",loginuser);
+router.post("/add", auth, regi_user); // Only admin can add users
 // router.post("/change-password", auth, changePassword);
 
-// router.get("/getusers", auth,  getuser); //  Only admin can get users
-// router.get("/getById/:_id", auth, getbyId);
-// router.put("/update/:_id", auth, Update);
-// router.delete("/delete/:_id", auth, Delete);
+router.get("/getusers", auth,  getuser); //  Only admin can get users
+router.get("/getById/:_id", auth, getbyId);
+router.put("/update/:_id", auth, Update);
+router.delete("/delete/:_id", auth, Delete);
 
 module.exports = router;
